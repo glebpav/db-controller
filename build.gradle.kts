d96a22cc687bb071295263c66ed1a3be00dedaa4
hlebnoe_pole <glebpavlyuk@gmail.com>
2025-03-02 22:31:32 +0300

    feat: add project files

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+}
+
+group = "ru.mephi"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    // https://mvnrepository.com/artifact/org.projectlombok/lombok
+    implementation("org.projectlombok:lombok:1.18.30")
+
+    // https://mvnrepository.com/artifact/com.google.dagger/dagger
+    implementation("com.google.dagger:dagger:2.55")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
