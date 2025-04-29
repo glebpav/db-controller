ced988a526492e6a5bc89cd1856225bf49c035b5
hlebnoe_pole <glebpavlyuk@gmail.com>
2025-04-29 03:20:51 +0300

    fix(test): remove new java features from tests

--- src/test/java/integration/testBoundary/TestInputBoundaryImpl.java
+++ src/test/java/integration/testBoundary/TestInputBoundaryImpl.java
@@ -3,15 +3,17 @@ package integration.testBoundary;
 import lombok.Getter;
 import ru.mephi.db.application.adapter.io.InputBoundary;
 
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class TestInputBoundaryImpl implements InputBoundary {
 
     @Getter
     private static final TestInputBoundaryImpl instance = new TestInputBoundaryImpl();
 
-    private final List<String> inputList;
+    private final Deque<String> inputList;
 
     private TestInputBoundaryImpl() {
         inputList = new LinkedList<>();
